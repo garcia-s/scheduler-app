b534e94a5c822ad3d541d94c9d0724d1179a9f07
Juan Garcia <garciajj18@gmail.com>
2022-09-16 04:56:04 -0500

    Mensaje

--- backend/modules/scheduling/_domain/aggregates/service_entity.ts
+++ backend/modules/scheduling/_domain/aggregates/service_entity.ts
@@ -2,5 +2,10 @@ import { Entity } from "../../../../core/interfaces/entity";
 
 
 export class ServiceEntity extends Entity {
+    private _id: string;
 
+
+    get id(): string {
+        return this._id;
+    }
 }
