dd1b2e84d5e4cd6fac855a4fb37d3fb0cae4d117
Juan Garcia <garciajj18@gmail.com>
2022-10-02 10:24:03 -0500

    Before Patas

--- backend/modules/scheduling/_domain/aggregates/service_aggregate.ts
+++ backend/modules/scheduling/_domain/aggregates/service_aggregate.ts
@@ -1,8 +1,11 @@
+import { Result } from "ts-results";
 import { UnimplementedError } from "../../../../core/errors/general";
+import { IDomainFailure } from "../../../../core/failures/interfaces";
 import Aggregate from "../../../../core/interfaces/aggregate";
 import { ServiceEntity } from "../entities/service_entity";
 import { ServiceProviderEntity } from "../entities/service_provider_entity";
 import { WorkspaceEntity } from "../entities/workspace_entity";
+import { AppointmentRequest } from "../value_objects/appointment_request";
 
 export default class ServiceAggregate extends Aggregate<ServiceEntity> {
   private _serviceProviders: ServiceProviderEntity[];
@@ -14,39 +17,44 @@ export default class ServiceAggregate extends Aggregate<ServiceEntity> {
     workSpaces: WorkspaceEntity[];
   }) {
     super(params.service);
-    this._serviceProviders = params.serviceProviders
+    this._serviceProviders = params.serviceProviders;
     this._workSpaces = params.workSpaces;
   }
-  
+
   public static create(params: {}) {
-    throw new UnimplementedError()
+    throw new UnimplementedError();
   }
 
   public static reconstitue() {
-    throw new UnimplementedError()
+    throw new UnimplementedError();
   }
 
-  addWorkspace(workspace: WorkspaceEntity) {
-    throw new UnimplementedError()
+  public addWorkspace(workspace: WorkspaceEntity) {
+    throw new UnimplementedError();
   }
 
-  removeWorkspace(id: string) {
-    throw new UnimplementedError()
+  public removeWorkspace(id: string) {
+    throw new UnimplementedError();
   }
 
-  addServiceProvider(serviceProvider: string): void {
-    throw new UnimplementedError()
+  public addServiceProvider(serviceProvider: string): void {
+    throw new UnimplementedError();
   }
 
-  removeServiceProvider(): void {
-    throw new UnimplementedError()
+  public removeServiceProvider(): void {
+    throw new UnimplementedError();
   }
-
-  scheduleAppointment(appointment: string): void {
+  
+  public scheduleAppointment(request: AppointmentRequest): Result<void, IDomainFailure> {
     
   }
 
-  rescheduleAppointment(): void {
-    throw new UnimplementedError()
+  public rescheduleAppointment(
+    request: AppointmentRequest,
+    appointmentId: string
+  ): void {
+    throw new UnimplementedError();
   }
+
+  private findAvailableServiceProvider(): Result<ServiceProviderEntity, IDomainFailure> {}
 }
--- backend/modules/scheduling/_domain/value_objects/appointment_request.ts
+++ backend/modules/scheduling/_domain/value_objects/appointment_request.ts
@@ -1,12 +1,35 @@
 import ValueObject from "../../../../core/interfaces/value_object";
 import { Timeframe } from "./timeframe";
 
-export type IAppointmentRequest = {
-  timeFrame: Timeframe,
+export type IAppointmentRequestParams = {
+  timeFrame: Timeframe;
   clientId: string;
-  providerId: string;
+  providerId?: string;
 };
 
-class AppointmentRequest extends ValueObject<IAppointmentRequest> {
-   
+export class AppointmentRequest extends ValueObject<IAppointmentRequestParams> {
+
+  private constructor(params: IAppointmentRequestParams) {
+    super(params);
+  }
+
+  create(params: IAppointmentRequestParams): AppointmentRequest {
+    return new AppointmentRequest(params);
+  }
+
+  reconstitute(params: IAppointmentRequestParams) :AppointmentRequest{
+    return new AppointmentRequest(params)
+  }
+
+  get timeFrame() : Timeframe{
+    return this._value.timeFrame
+  }
+
+  get clientId(): string {
+    return this._value.clientId
+  }
+
+  get providerId(): string {
+    return this._value.providerId;
+  }
 }
